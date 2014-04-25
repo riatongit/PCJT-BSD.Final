11bb7f4327b8481ce358ed1dfaf49bea1b6c6b18
riatongit <riat.inf@gmail.com>
2014-04-25 11:21:06 -0700

    @Naveen Created system class
    class RIAT{
    /*
    * @ encapsulated class with getters
    * @ with system information (RIAT info)
    */
    }
    
    created pkg.test
    to add testing classes and frames

---
+++ PCJTBSDFinalProject/src/riat/RIAT.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riat;
+
+/**
+ *
+ * @author Naveen Dinuka
+ */
+public final class RIAT {
+
+    private final String nic = "123456789v";
+    private final String first_name = "RESONANCE";
+    private final String last_name = "TECHNOLOGY";
+    private final int mobile = 778066593;
+
+    private final String username = "system.riat";
+    private final String password = "riat.sys";
+
+    /**
+     * @return the nic
+     */
+    public String getNic() {
+        return nic;
+    }
+
+    /**
+     * @return the first_name
+     */
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    /**
+     * @return the last_name
+     */
+    public String getLast_name() {
+        return last_name;
+    }
+
+    /**
+     * @return the mobile
+     */
+    public int getMobile() {
+        return mobile;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ PCJTBSDFinalProject/src/test/testRIAT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import riat.RIAT;
+
+/**
+ *
+ * @author Naveen Dinuka
+ */
+public class testRIAT {
+
+    public static void main(String[] args) {
+        RIAT r = new RIAT();
+        System.out.println(r.getFirst_name());
+    }
+
+}
